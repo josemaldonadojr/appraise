318409cc1516909071fa7c5b6e904d1531cd1f37
Jose <josemaldonadobusiness@gmail.com>
2025-09-22 22:11:06 -0500

    configure socialProviders

--- convex/auth.ts
+++ convex/auth.ts
@@ -28,6 +28,12 @@ export const createAuth = (
       enabled: true,
       requireEmailVerification: false,
     },
+    socialProviders: {
+      google: {
+        clientId: process.env.GOOGLE_CLIENT_ID as string,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      }
+    },
     plugins: [
       // The Convex plugin is required for Convex compatibility
       convex(),
