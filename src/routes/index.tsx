c934b6522597d45f5c8f6a66c09de727d4965c6c
Jose <josemaldonadobusiness@gmail.com>
2025-09-22 23:05:29 -0500

    add authentication flow to home route

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,41 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { api } from '../../convex/_generated/api'
-import { useMutation } from 'convex/react'
-import { useSuspenseQuery } from '@tanstack/react-query'
-import { convexQuery } from '@convex-dev/react-query'
 import { Signup } from '@/components/signup'
+import {
+  Authenticated,
+  Unauthenticated,
+  AuthLoading,
+} from "convex/react";
 
 export const Route = createFileRoute('/')({
   component: Home,
 })
 
 function Home() {
-  const {
-    data: { viewer, numbers },
-  } = useSuspenseQuery(convexQuery(api.myFunctions.listNumbers, { count: 10 }))
-
-  const addNumber = useMutation(api.myFunctions.addNumber)
-
   return (
-    <Signup />
-  )
-}
-
-function ResourceCard({
-  title,
-  description,
-  href,
-}: {
-  title: string
-  description: string
-  href: string
-}) {
-  return (
-    <div className="flex flex-col gap-2 bg-slate-200 dark:bg-slate-800 p-4 rounded-md h-28 overflow-auto">
-      <a href={href} className="text-sm underline hover:no-underline">
-        {title}
-      </a>
-      <p className="text-xs">{description}</p>
-    </div>
+    <>
+        <Authenticated>
+          <div>Authenticated</div>
+        </Authenticated>
+        <Unauthenticated>
+          <Signup />
+        </Unauthenticated>
+        <AuthLoading>
+          <div>AuthLoading</div>
+        </AuthLoading>
+    </>
   )
 }
